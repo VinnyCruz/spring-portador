9d8af0441177589ecc576501270e87184a024155
Vinicius Cruz <vinicius.cruz@jazztech.corp>
2023-07-06 14:04:12 -0300

    correção de erro

--- src/main/java/org/jazztech/portador/mapper/LimitUpdateResponseMapper.java
+++
@@ -1,8 +0,0 @@
-package org.jazztech.portador.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface LimitUpdateResponseMapper {
-    LimitUpdateResponse from(LimitUpdateRequest limitUpdateRequest);
-}
